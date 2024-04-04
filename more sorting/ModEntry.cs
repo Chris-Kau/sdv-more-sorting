e9c018058fb7e233d2d8b85947d8d9b5a015cc7c
Chris-Kau <uhchris04@gmail.com>
2024-04-03 23:06:19 -0700

    Add project files.

---
+++ more sorting/ModEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Objects;
+
+namespace more_sorting
+{
+    internal sealed class ModEntry : Mod
+    {
+        //Farmer person = Game1.player;
+        public override void Entry(IModHelper help)
+        {
+
+            Helper.Events.Display.MenuChanged += this.sortItems;
+        }
+
+        private void sortItems(object? sender, MenuChangedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+            List<Item> containerContents = accessChest.getItems(sender, e);
+            foreach(Item i in containerContents)
+            {
+                this.Monitor.Log($"{i.Name}", LogLevel.Debug);
+            }
+
+        }
+
+    }
+}
---
+++ more sorting/getItems.cs
@@ -0,0 +1,33 @@
+﻿using StardewModdingAPI.Events;
+using StardewModdingAPI;
+using StardewValley.Objects;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace more_sorting
+{
+    static class accessChest
+    {
+        internal static List<Item> getItems(object? sender, MenuChangedEventArgs e)
+        {
+            List<Item> containerContents = new List<Item>();
+            if (e.NewMenu is StardewValley.Menus.ItemGrabMenu menu)
+            {
+                //chest, includes stone chest and big chests
+                if (menu.sourceItem is Chest chest)
+                {
+                    foreach (Item i in chest.Items)
+                    {
+                        containerContents.Add(i);
+                    }
+                }
+            }
+            return containerContents;
+        }
+    }
+}
