02b6f07e35a777c193ff4b8115334651cfd7e1fc
Chris-Kau <uhchris04@gmail.com>
2024-04-08 00:03:20 -0700

    sorts items alphabetically using bubblesort

--- more sorting/AccessChest.cs
+++ more sorting/AccessChest.cs
@@ -12,7 +12,7 @@ using System.Runtime.CompilerServices;
 namespace more_sorting
 {
     //I eat bleh
-    static class accessChest
+    static class ChestMethods
     {
         internal static List<Item> getItems(object? sender, Chest chest)
         {
@@ -20,6 +20,15 @@ namespace more_sorting
             foreach (Item i in chest.Items)
                 containerContents.Add(i);
             return containerContents;
+        }
+
+        internal static void ReplaceContents(object? sender, List<Item> ItemsList, StardewValley.Menus.ItemGrabMenu menu)
+        {
+            menu.ItemsToGrabMenu.actualInventory.Clear();
+            foreach(Item i in ItemsList)
+            {
+                menu.ItemsToGrabMenu.actualInventory.Insert(0, i);
+            }
         }
     }
 }
--- more sorting/SortOptions.cs
+++ more sorting/SortOptions.cs
@@ -4,6 +4,7 @@ using StardewValley.Objects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,22 +12,41 @@ namespace more_sorting
 {
     static class SortOptions
     {
+        private static void BubbleSort(List<Item> items)
+        {
+            int n = items.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    int result = string.Compare(items[j].Name, items[j + 1].Name);
+                    //if result < 0, that means that string j comes before string j+1 alphabetically
+                    //if result > 0, then that means string j comes after string j+1 alphabetically
+                    //if result == 0, then the strings are the same.
+                    if (result < 0)
+                    {
+                        Item temp = items[j];
+                        items[j] = items[j + 1];
+                        items[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+
         //TODO: sorts items alphabetically A -> Z
         internal static void AlphaSort(object? sender, Chest chest, StardewValley.Menus.ItemGrabMenu menu)
         {
             //gets the items in the chest the player is currently in
-            List<Item> containerContents = accessChest.getItems(sender, chest);
-            //prints all the items into the console
-            foreach (Item i in containerContents)
-            {
-                Console.WriteLine($"{i.Name}");
-            }
+            List<Item> chestContents = ChestMethods.getItems(sender, chest);
 
-            //clears the chest (just for testing purposes)
-            for (int i = 0; i < containerContents.Count; i++)
-            {
-                menu.ItemsToGrabMenu.actualInventory.RemoveAt(0);
-            }
+            //TODO: sort the containerConents lists HERE vvvvv
+            //bubble sort for test, but we should 100% not use bubble sort 
+            BubbleSort(chestContents);
+            //replaces the contents of the chest with the sorted items:
+            ChestMethods.ReplaceContents(sender, chestContents, menu);
         }
+
+
     }
 }
