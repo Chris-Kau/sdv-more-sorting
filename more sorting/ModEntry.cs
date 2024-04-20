4bfac7a342e084a8a07ce149745b9e7f90075e72
Chris-Kau <uhchris04@gmail.com>
2024-04-19 20:59:23 -0700

    added new option to sort by price, uses selection sort now

--- more sorting/ModEntry.cs
+++ more sorting/ModEntry.cs
@@ -5,12 +5,14 @@ using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
 using StardewValley.Objects;
-
+using StardewValley.Menus;
+using Microsoft.Xna.Framework.Graphics;
 namespace more_sorting
 {
     internal sealed class ModEntry : Mod
     {
         Farmer person = Game1.player;
+
         public override void Entry(IModHelper help)
         {
             //O keybind should sort items alphabetically
@@ -22,17 +24,27 @@ namespace more_sorting
             if (!Context.IsWorldReady)
                 return;
             //tests to see if the player clicked O while in a clickable menu
-            if ((e.Button is SButton.O) && (Game1.activeClickableMenu is StardewValley.Menus.ItemGrabMenu menu))
-            { 
+            if((Game1.activeClickableMenu is StardewValley.Menus.ItemGrabMenu menu))
+            {
                 //checks to see if that clickable menu is a chest
-                if(menu.sourceItem is Chest chest)
+                if (menu.sourceItem is Chest chest)
                 {
-                    SortOptions.AlphaSort(sender, chest, menu);
+                    //sorts alphabetically if user clicks O
+                    if ((e.Button is SButton.O))
+                    {
+                        SortOptions.AlphaSort(sender, chest, menu);
+
+                    }
+                    //sorts by price if user clicks I
+                    else if (e.Button is SButton.I)
+                    {
+                        SortOptions.PriceSort(sender, chest, menu);
+                    }
                 }
 
             }
-
         }
+
 
     }
 }
--- more sorting/SortOptions.cs
+++ more sorting/SortOptions.cs
@@ -12,38 +12,87 @@ namespace more_sorting
 {
     static class SortOptions
     {
-        private static void BubbleSort(List<Item> items)
+        private static void BubbleSort(List<Item> items, string method)
         {
             int n = items.Count;
             for (int i = 0; i < n - 1; i++)
             {
                 for (int j = 0; j < n - i - 1; j++)
                 {
-                    int result = string.Compare(items[j].Name, items[j + 1].Name);
-                    //if result < 0, that means that string j comes before string j+1 alphabetically
-                    //if result > 0, then that means string j comes after string j+1 alphabetically
-                    //if result == 0, then the strings are the same.
-                    if (result < 0)
+                    if (method == "alpha")
                     {
-                        Item temp = items[j];
-                        items[j] = items[j + 1];
-                        items[j + 1] = temp;
+                        int result = string.Compare(items[j].Name, items[j + 1].Name);
+                        //if result < 0, that means that string j comes before string j+1 alphabetically
+                        //if result > 0, then that means string j comes after string j+1 alphabetically
+                        //if result == 0, then the strings are the same.
+                        if (result < 0)
+                        {
+                            Item temp = items[j];
+                            items[j] = items[j + 1];
+                            items[j + 1] = temp;
+                        }
+                    }else if(method == "price")
+                    {
+                        if (items[j].sellToStorePrice() > items[j+1].sellToStorePrice())
+                        {
+                            Item temp = items[j];
+                            items[j] = items[j+1];
+                            items[j + 1] = temp;
+                        }
                     }
+
                 }
             }
         }
 
+        private static void SelectionSort(List<Item> items, string method)
+        {
+            int n = items.Count;
+            for(int i = 0; i < n - 1; i++)
+            {
+                int minidx = i;
+                for(int j = i + 1; j < n; j++)
+                {
+                    if(method == "alpha")
+                    {
+                        int result = string.Compare(items[minidx].Name, items[j].Name);
+                        //if result < 0, that means that string j comes before string j+1 alphabetically
+                        //if result > 0, then that means string j comes after string j+1 alphabetically
+                        //if result == 0, then the strings are the same.
+                        if (result < 0)
+                        {
+                            minidx = j;
+                        }
+                    }
+                    if(method == "price")
+                    {
+                        if (items[j].sellToStorePrice() < items[minidx].sellToStorePrice())
+                        {
+                            minidx = j;
+                        }
+                    }
+                }
+                Item temp = items[minidx];
+                items[minidx] = items[i];
+                items[i] = temp;
+            }
+        }
 
-        //TODO: sorts items alphabetically A -> Z
+
         internal static void AlphaSort(object? sender, Chest chest, StardewValley.Menus.ItemGrabMenu menu)
         {
             //gets the items in the chest the player is currently in
             List<Item> chestContents = ChestMethods.getItems(sender, chest);
-
-            //TODO: sort the containerConents lists HERE vvvvv
             //bubble sort for test, but we should 100% not use bubble sort 
-            BubbleSort(chestContents);
+            SelectionSort(chestContents, "alpha");
             //replaces the contents of the chest with the sorted items:
+            ChestMethods.ReplaceContents(sender, chestContents, menu);
+        }
+
+        internal static void PriceSort(object? sender, Chest chest, StardewValley.Menus.ItemGrabMenu menu)
+        {
+            List<Item> chestContents = ChestMethods.getItems(sender, chest);
+            SelectionSort(chestContents, "price");
             ChestMethods.ReplaceContents(sender, chestContents, menu);
         }
 
