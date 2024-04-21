7d1d78862afcb8cf05cbaa5cf351206aea56bf81
Chris-Kau <uhchris04@gmail.com>
2024-04-20 22:07:40 -0700

    Organized functions/classes, fixed window resizing issue that caused sort buttons to disappear

--- more sorting/ModEntry.cs
+++ more sorting/ModEntry.cs
@@ -15,144 +15,58 @@ namespace more_sorting
     internal sealed class ModEntry : Mod
     {
         Farmer person = Game1.player;
-        Vector2 AlphaSortIconPos;
-        Rectangle AlphaSortIconArea;
-        Vector2 PriceSortIconPos;
-        Rectangle PriceSortIconArea;
-        //the ? makes it so PriceSortIcon is nullable if not assigned to anything
-        ClickableTextureComponent? PriceSortIcon;
-        ClickableTextureComponent? AlphaSortIcon;
+
         public override void Entry(IModHelper helper)
         {
-            //O keybind should sort items alphabetically
-            Helper.Events.Input.ButtonPressed += this.Keybinds;
             //Adds Button for the user to "click"
-            Helper.Events.Display.RenderedActiveMenu += this.AddSortButtons;
+            Helper.Events.Display.RenderedActiveMenu += this.HoverEffect;
             //Gives the Illusion of the user "clicking" on the buttons
             Helper.Events.Input.ButtonPressed += this.ClickedSortButtons;
+            //Updates the button everytime the player opens a chest
+            Helper.Events.Display.MenuChanged += this.CreateButtons;
+            //Updates the button pos evertime the player resizes the window, so the buttons do not go off screen.
+            Helper.Events.Display.WindowResized += this.RecreateButtons;
         }
 
-        private void Keybinds(object? sender, ButtonPressedEventArgs e)
+        private void RecreateButtons(object? sender, WindowResizedEventArgs e)
         {
-            if (!Context.IsWorldReady)
-                return;
-            //tests to see if the player clicked O while in a clickable menu
-            if((Game1.activeClickableMenu is StardewValley.Menus.ItemGrabMenu menu))
+            if(Game1.activeClickableMenu is ItemGrabMenu menu)
             {
-                //checks to see if that clickable menu is a chest
-                if (menu.sourceItem is Chest chest)
+                if(menu.sourceItem is Chest chest)
                 {
-                    //sorts alphabetically if user clicks O
-                    if ((e.Button is SButton.O))
-                    {
-                        SortOptions.AlphaSort(sender, chest, menu);
-
-                    }
-                    //sorts by price if user clicks I
-                    else if (e.Button is SButton.I)
-                    {
-                        SortOptions.PriceSort(sender, chest, menu);
-                    }
+                    SortButtonMethods.MakeAlphaIcon(menu, Helper.ModContent.Load<Texture2D>("./assets/AlphaSortIcon.png"));
+                    SortButtonMethods.MakePriceIcon(menu, Helper.ModContent.Load<Texture2D>("./assets/PriceSortIcon.png"));
                 }
+            }
 
+        }
+
+        private void CreateButtons(object? sender, MenuChangedEventArgs e)
+        {
+            if(e.NewMenu is ItemGrabMenu menu)
+            {
+                if(menu.sourceItem is Chest chest)
+                {
+                    SortButtonMethods.MakeAlphaIcon(menu, Helper.ModContent.Load<Texture2D>("./assets/AlphaSortIcon.png"));
+                    SortButtonMethods.MakePriceIcon(menu, Helper.ModContent.Load<Texture2D>("./assets/PriceSortIcon.png"));
+                }
             }
         }
-
-        private void AddSortButtons(object? sender, RenderedActiveMenuEventArgs e)
+        private void HoverEffect(object? sender, RenderedActiveMenuEventArgs e)
         {
             if(Game1.activeClickableMenu is ItemGrabMenu menu)
             {
                 //get mouse position for later uses
                 Vector2 mousePosition = new Vector2(Game1.getMouseXRaw(), Game1.getMouseYRaw());
-
-                //Only create one instance so we can edit its .scale property for a smoother transition, otherwise it'll keep creating a ClickableComponentTexture w scale 1 when we 
-                //are trying to edit the scale
-                if (AlphaSortIcon is null)
+                //these are in charge of drawing the icons as well as making a scale up/down effect when hovering.
+                if (SortButtonMethods.PriceSortIcon is not null && SortButtonMethods.AlphaSortIcon is not null)
                 {
-                    //Responsible for creating the button and drawing it to the screen 
-                    AlphaSortIcon = new ClickableTextureComponent(
-                        new Rectangle((int)menu.organizeButton.getVector2().X + 16 + 64, (int)menu.organizeButton.getVector2().Y, 64, 64),
-                        Helper.ModContent.Load<Texture2D>("./assets/AlphaSortIcon.png"),
-                        new Rectangle(0, 0, 64, 64),
-                        1f
-                        ); 
-                    //Sets the Icon to be in the position to the right of the organize button
-                    AlphaSortIconPos = new Vector2(menu.organizeButton.getVector2().X + 16 + 64, menu.organizeButton.getVector2().Y);
-                    //Makes a new rectangle which is then used to check if the mouse is in that area in ClickedAlphaSort()
-                    AlphaSortIconArea = new Rectangle((int)AlphaSortIconPos.X, (int)AlphaSortIconPos.Y, 64, 64);
+                    SortButtonMethods.HoverEffect(SortButtonMethods.PriceSortIcon, (int)mousePosition.X, (int)mousePosition.Y, 1f, 1.1f, 0.02f);
+                    SortButtonMethods.HoverEffect(SortButtonMethods.AlphaSortIcon, (int)mousePosition.X, (int)mousePosition.Y, 1f, 1.1f, 0.02f);
                 }
-
-                //Repeated process but for PriceSortIcon, this time to the right of the Quick Stack to Existing Slots button
-                
-                if (PriceSortIcon is null)
-                {
-                    PriceSortIcon = new ClickableTextureComponent(
-                         new Rectangle((int)menu.fillStacksButton.getVector2().X + 16 + 64, (int)menu.fillStacksButton.getVector2().Y, 64, 64),
-                         Helper.ModContent.Load<Texture2D>("./assets/PriceSortIcon.png"),
-                         new Rectangle(0, 0, 64, 64),
-                         1f
-                         );
-                    PriceSortIconPos = new Vector2(menu.fillStacksButton.getVector2().X + 16 + 64, menu.fillStacksButton.getVector2().Y);
-                    PriceSortIconArea = new Rectangle((int)PriceSortIconPos.X, (int)PriceSortIconPos.Y, 64, 64);
-                }
- 
-
-                //these if statements are in charge of drawing the icons as well as making a scale up/down effect when hovering.
-                if(AlphaSortIcon.containsPoint((int)mousePosition.X, (int)mousePosition.Y))
-                {
-                    ScaleTransition(AlphaSortIcon, 1.1f, 0.02f);
-                    AlphaSortIcon.draw(Game1.spriteBatch, Color.White, 0f);
-                }
-                else//when the user stops hovering the button, scale it to its original size
-                {
-                    ScaleTransition(AlphaSortIcon, 1f, -0.02f);
-                    AlphaSortIcon.draw(Game1.spriteBatch, Color.White, 0f);
-                }
-
-
-                if(PriceSortIcon.containsPoint((int)mousePosition.X, (int)mousePosition.Y))
-                {
-                    ScaleTransition(PriceSortIcon, 1.1f, 0.02f);
-                    PriceSortIcon.draw(Game1.spriteBatch, Color.White, 0f);
-                }
-                else
-                {
-                    ScaleTransition(PriceSortIcon, 1f, -0.02f);
-                    PriceSortIcon.draw(Game1.spriteBatch, Color.White, 0f);
-                }
-
                 //Draws the mouse so the cursor is not under the button
                 menu.drawMouse(Game1.spriteBatch);
-
             }
-        }
-
-        private void ScaleTransition(ClickableTextureComponent icon, float scaleResult, float speed)
-        {
-            //if speed > 0, we want to scale up, else scale down
-            if(speed > 0)
-            {
-                if (icon.scale < scaleResult)
-                {
-                    icon.scale += speed;
-                }
-                else
-                {
-                    icon.scale = scaleResult;
-                }
-            }
-            else
-            {
-                if (icon.scale > scaleResult)
-                {
-                    icon.scale += speed;
-                }
-                else
-                {
-                    icon.scale = scaleResult;
-                }
-            }
-
         }
         private void ClickedSortButtons(object? sender, ButtonPressedEventArgs e)
         {
@@ -163,12 +77,12 @@ namespace more_sorting
                 if(menu.sourceItem is Chest chest)
                 {
                     //Checks to see if the player clicks on the AlphaSortIcon, and if the player does, sort it Alphabetically
-                    if (AlphaSortIconArea.Contains(mousePosition.X, mousePosition.Y))
+                    if (SortButtonMethods.AlphaSortIconArea.Contains(mousePosition.X, mousePosition.Y))
                     {
                         SortOptions.AlphaSort(sender, chest, menu);
                     }
                     //Checks to see if the player clicks on the PriceSortIcon
-                    else if (PriceSortIconArea.Contains(mousePosition.X, mousePosition.Y))
+                    else if (SortButtonMethods.PriceSortIconArea.Contains(mousePosition.X, mousePosition.Y))
                     {
                         SortOptions.PriceSort(sender, chest, menu);
                     }
---
+++ more sorting/SortButtonMethods.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValley;
+using StardewValley.Menus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace more_sorting
+{
+    public class SortButtonMethods
+    {
+        public static Vector2 AlphaSortIconPos;
+        public static Rectangle AlphaSortIconArea;
+        public static Vector2 PriceSortIconPos;
+        public static Rectangle PriceSortIconArea;
+        public static ClickableTextureComponent? AlphaSortIcon;
+        public static ClickableTextureComponent? PriceSortIcon;
+
+        internal static void MakeAlphaIcon(ItemGrabMenu menu, Texture2D img)
+        {
+            //Responsible for creating the button and drawing it to the screen 
+            AlphaSortIcon = new ClickableTextureComponent(
+                new Rectangle((int)menu.organizeButton.getVector2().X + 16 + 64, (int)menu.organizeButton.getVector2().Y, 64, 64),
+                img,
+                new Rectangle(0, 0, 64, 64),
+                1f
+                );
+            //Gets position of the button as a vector
+            AlphaSortIconPos = new Vector2(menu.organizeButton.getVector2().X + 16 + 64, menu.organizeButton.getVector2().Y);
+            //uses that position to create a rectangle around that button, used for hover animations and detecting clicks
+            AlphaSortIconArea = new Rectangle((int)AlphaSortIconPos.X, (int)AlphaSortIconPos.Y, 64, 64);
+        }
+
+        internal static void MakePriceIcon(ItemGrabMenu menu, Texture2D img)
+        {
+            PriceSortIcon = new ClickableTextureComponent(
+                    new Rectangle((int)menu.fillStacksButton.getVector2().X + 16 + 64, (int)menu.fillStacksButton.getVector2().Y, 64, 64),
+                    img,
+                    new Rectangle(0, 0, 64, 64),
+                    1f
+                    );
+            PriceSortIconPos = new Vector2(menu.fillStacksButton.getVector2().X + 16 + 64, menu.fillStacksButton.getVector2().Y);
+            PriceSortIconArea = new Rectangle((int)PriceSortIconPos.X, (int)PriceSortIconPos.Y, 64, 64);
+        }
+        internal static void HoverEffect(ClickableTextureComponent icon, int x, int y, float originalScale, float scaleResult, float delta)
+        {
+            //Checks to see if the mouse cursor is on the button, if it is, scale the button up, but if it isnt, scale the button to its original size
+            if (icon.containsPoint(x,y))
+            {
+                ScaleTransition(icon, scaleResult, delta);
+                DrawButton(icon);
+            }
+            else
+            {
+                ScaleTransition(icon, originalScale, -delta);
+                DrawButton(icon);
+            }
+        }
+
+        private static void DrawButton(ClickableTextureComponent icon)
+        {
+            icon.draw(Game1.spriteBatch, Color.White, 0.99f);
+        }
+
+        private static void ScaleTransition(ClickableTextureComponent icon, float scaleResult, float delta)
+        {
+            //if delta > 0, that means we want to scale up, otherwise scale down
+            if(delta > 0)
+            {
+                if(icon.scale < scaleResult)
+                {
+                    icon.scale += delta;
+                }
+                else
+                {
+                    icon.scale = scaleResult;
+                }
+            }
+            else
+            {
+                if(icon.scale > scaleResult)
+                {
+                    icon.scale += delta;
+                }
+                else
+                {
+                    icon.scale = scaleResult;
+                }
+            }
+        }
+    }
+}
